d366e0c7a4779a1a38ad580b16be33cb3c557096
zhaowx <643948989@qq.com>
2021-03-12 08:58:34 +0800

    first commit

---
+++ gulimall-member/src/test/java/top/lemcoo/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lemcoo.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
